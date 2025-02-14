a4bdb335cd460347a40987959651bc7eb85c628c
pankaj kumar <117341395+pk11345@users.noreply.github.com>
2025-02-14 23:25:05 +0530

    working on UI

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -6,7 +6,7 @@ const Welcome = () => {
     <>
     <div className='p-7 w-full h-[400px] flex justify-center items-center'>
         <h1 className='flex flex-col items-center text-3xl font-semibold text-white bg-black p-10 rounded-[30px]'>Welcome! 
-          <span>New User? <Link to="/signup">Signup</Link></span> 
+          <span className='flex'>New User? <Link to="/signup">Signup</Link></span> 
           <span className='flex'>Already an User? <Link to="/login">Login</Link></span> </h1>
     </div>
     </>
